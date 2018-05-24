f647d0a144dd83eb578f7b0e537fcaf8a3040243
Anton Castell <castellanton@gmail.com>
2018-05-24 15:57:31 +0200

    added expanding map to homepage

---
+++ source/javascripts/components/banner.js
@@ -0,0 +1,43 @@
+// function mapExpand(){
+  const button = document.querySelector('.btn')
+  const banner = document.querySelector('.banner')
+  console.log(button)
+  console.log(banner)
+
+  banner_button.addEventListener("click", (event) => {
+    banner.classList.remove('inactive')
+    banner.classList.add('active')
+  });
+// };
+
+
+
+
+
+
+// Vorlage
+// function navbarHide(){
+//   let navbar_button = document.querySelector(".navbar-opener");
+//   if (navbar_button){
+//     navbar_button.addEventListener("click",(event)=>{
+//       console.log("test");
+//       let navbar = document.querySelector(".navbar-wagon");
+//       navbar.classList.remove("hidden");
+//       navbar.classList.remove("fadeOutUp");
+
+//     })
+//   }
+
+//   let closer_button = document.querySelector(".navbar-closer");
+//   if (closer_button){
+//     closer_button.addEventListener("click",(event)=>{
+//       console.log("test");
+//       let navbar = document.querySelector(".navbar-wagon");
+//       navbar.classList.toggle("fadeOutUp");
+
+//     })
+//   }
+
+// }
+
+// export { navbarHide }
